e877c7d1b4ab8381987c68d42ca825e7f95ae03b
Csaba Sallai <68144659+csabika98@users.noreply.github.com>
2021-02-15 09:21:08 +0100

    merged from V2

--- server.py
+++ server.py
@@ -1,12 +1,206 @@
-from flask import Flask
+from re import sub
+from typing import NamedTuple
+from flask import Flask, render_template, redirect, request, url_for, flash
+from flask.helpers import get_flashed_messages
+import database_common
+import psycopg2
+import psycopg2.extras
+from psycopg2.extras import DictConnection, DictCursor, NamedTupleCursor, RealDictCursor
+import data_manager
+from werkzeug.utils import secure_filename
+import os
 
+UPLOAD_FOLDER = 'static/images/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def list_the_questions():
+    list_questions = data_manager.show_answers()
+    
+    list_id = data_manager.show_id()
+    list_tags = data_manager.list_all_tags(list_id)
+
+    city = request.args.get('sort-option')
+    tags = request.args.get('search-tag')
+    search_opt = request.args.get('search')
+
+    if city:
+        list_questions = data_manager.show_answers(city)
+    elif tags and not search_opt:
+        list_questions = data_manager.search_tag(tags)
+    elif search_opt and not tags:
+        list_questions = data_manager.search_title(search_opt)
+    elif tags and search_opt:
+        list_questions = data_manager.search_title_tag(search_opt,tags)
+    else:
+        list_questions = data_manager.show_answers()
+
+    return render_template("index.html",list_tags=list_tags , list_questions=list_questions)
+
+
+@database_common.connection_handler
+def create_question(cursor: RealDictCursor, primary_key:str , title: str, message: str, submission_time: str, vote_number: int, view_number: int, image:str) -> list:
+    cursor.execute("INSERT INTO question (id, title, message, submission_time, vote_number, view_number, image) VALUES (%s,%s,%s,%s, %s, %s, %s)", (primary_key, title, message, submission_time, vote_number, view_number, image))
+
+@database_common.connection_handler
+def add_tag(cursor: RealDictCursor, question_id: str, tag_id: str) -> list:
+    cursor.execute("INSERT INTO question_tag (question_id,tag_id) VALUES (%s,%s)", (question_id,tag_id))
+
+@database_common.connection_handler
+def update_tag(cursor: RealDictCursor, question_id: str, tag_id: str) -> list:
+    cursor.execute("""UPDATE question_tag SET question_id = %s, tag_id = %s WHERE question_id = %s""", (question_id,tag_id,question_id))
+
+
+@app.route("/add_questions/", methods=["GET", "POST"])
+def add_questions():
+    list_tag = data_manager.list_tags()
+    prima_key = data_manager.show_primary_key()
+    if request.method == "POST":
+        tit = request.form.get("title")
+        mess = request.form.get("message")
+        sub_time = request.form.get("submission_time")
+        file_name = "default.png"
+        uploaded_image = request.files['image']
+        if uploaded_image.filename != "":
+            uploaded_image.save(os.path.join(app.config['UPLOAD_FOLDER'], uploaded_image.filename))
+            file_name = uploaded_image.filename
+        tags = request.form["tags"]
+        vot_num = 0
+        view_num = 0
+        create_question(prima_key , tit, mess, sub_time, vot_num, view_num, file_name)
+        add_tag(prima_key, tags)
+
+        return redirect("/")
+    return render_template("add_questions.html", prima_key=prima_key, list_tag=list_tag)
+
+
+@app.route("/edit_questions/<question_id>", methods=["GET", "POST"])
+def edit_questions(question_id=None):
+    list_question = data_manager.show_answer(question_id)
+    list_title = data_manager.show_title_only(question_id)
+    list_msg = data_manager.show_message_only(question_id)
+    list_tags = data_manager.list_tags()
+
+    if request.method == "POST":
+        submission_time = request.form["submission_time"]
+        message = request.form["message"] 
+        title = request.form["title"]
+        tag = request.form["tags"]
+        uploaded_image = request.files['image']
+        file_name = uploaded_image.filename
+        if uploaded_image.filename != "":
+            uploaded_image.save(os.path.join(app.config['UPLOAD_FOLDER'], uploaded_image.filename))
+        data_manager.edit_questions(file_name,submission_time, message, question_id, title)
+        update_tag(question_id, tag)
+        return redirect("/")
+    return render_template('edit_questions.html', question_id=question_id,list_tags=list_tags , list_question=list_question, list_title=list_title, list_msg=list_msg)
+
+
+@app.route("/display/<question_id>/vote_up/", methods=["GET", "POST"])
+def vote_up(question_id=None):
+
+    if request.method == "GET":
+        data_manager.update_question_vote(question_id, True)
+        return redirect(request.referrer)
+    return render_template("display.html", question_id=question_id, add_questions=add_questions, add_comment=add_comment)
+
+
+@app.route("/display/<question_id>/vote_down/", methods=["GET", "POST"])
+def vote_down(question_id=None):
+
+    if request.method == "GET":
+        data_manager.update_question_vote(question_id,False)
+        return redirect(request.referrer)
+
+    return render_template("display.html", question_id=question_id, add_questions=add_questions, add_comment=add_comment)
+
+
+@app.route("/display/<question_id>", methods=["POST", "GET"])
+def display_list(question_id=None, comment_id=None):
+    list_nested_comments = data_manager.nested_comments(question_id)
+    list_tags = data_manager.load_tags(question_id)
+    list_comment = data_manager.show_comment(question_id)
+    list_questions = data_manager.show_answer(question_id)
+    if request.method == "POST":
+        data_manager.update_view(question_id)
+  
+    return render_template("display.html",list_tags=list_tags, question_id=question_id, comment_id=comment_id, list_comments=list_comment, list_questions=list_questions, nested_comment=list_nested_comments)
+
+
+@app.route("/display/<comment_id>/delete", methods=["POST", "GET"])
+def delete_answer(comment_id=None):
+    if request.method == "GET":
+        data_manager.delete_comment(comment_id)
+        return redirect(request.referrer)
+    return render_template("display.html", comment_id=comment_id)
+
+
+@app.route("/delete/<question_id>", methods=["GET", "POST"])
+def delete(question_id=None):
+
+    if request.method == "GET":
+        data_manager.delete_question(question_id)
+        return redirect("/")
+    return render_template("index.html", question_id=question_id)
+
+
+@app.route("/comments/", methods=["POST", "GET"])
+def show_comments():
+    list_comments = data_manager.show_answers("answer")
+    com_table_header = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
+    return render_template("comments.html", list_comments=list_comments, com_table_header=com_table_header)
+
+
+
+@app.route("/comments/add/<question_id>", methods=["GET", "POST"])
+def add_comments(question_id=None):
+    list_questions = data_manager.show_message_only(question_id)
+    if request.method == "POST":
+        message = request.form["message"]
+        submission_time = request.form["submission_time"]
+        vote_number = 0
+        file_name = "default.png"
+        uploaded_image = request.files['image']
+        if uploaded_image.filename != "":
+            uploaded_image.save(os.path.join(app.config['UPLOAD_FOLDER'], uploaded_image.filename))
+            file_name = uploaded_image.filename
+        data_manager.add_new_answer(submission_time, vote_number, question_id, message, file_name)
+        return redirect("/display/" + question_id)
+    return render_template("add_comment.html", question_id=question_id, list_questions=list_questions)
+
+
+@app.route("/comments/<question_id>/<comment_id>/vote_up", methods=["GET", "POST"])
+def vote_comment_up(comment_id=None, question_id=None):
+    if request.method == "GET":
+        data_manager.update_comment_vote(comment_id,True) 
+        return redirect("/display/" + question_id)
+    return render_template("display.html", question_id=question_id)
+
+
+@app.route("/comments/<answer_id>/<question_id>/add/", methods=["GET", "POST"])
+def add_answer_to_answer(answer_id=None, question_id=None):
+    list_comments = data_manager.show_comment(question_id)
+    show_mess_only = data_manager.show_answer_mgs_only(question_id, answer_id)
+    if request.method == "POST":
+        message = request.form["message"]
+        submission_time = request.form["submission_time"]
+        data_manager.answer_to_answer(answer_id, question_id, message, submission_time)
+        return redirect("/display/" + question_id)
+    return render_template("add_answer_comment.html", question_id=question_id, answer_id=answer_id,list_comments=list_comments, show_mess_only=show_mess_only)
+
+
+
+@app.route("/comments/<question_id>/<comment_id>/vote_down", methods=["GET", "POST"])
+def vote_comment_down(comment_id=None, question_id=None):
+
+    if request.method == "GET":
+        data_manager.update_comment_vote(comment_id,False)
+        return redirect("/display/ " + question_id)
+    return render_template("display.html", question_id=question_id)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
